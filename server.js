ba07973256a8b844be0003cda738171c77d881a7
Kshitij Tiwari <jagadguru6969@gmail.com>
2018-05-27 01:05:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -68,6 +68,6 @@ io.on('connection', function (socket) {
   });
 });
 
-server.listen(8081, function () {
+server.listen(process.env.PORT || 5000, function () {
   console.log(`Listening on ${server.address().port}`);
 });
